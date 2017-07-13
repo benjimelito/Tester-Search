5d7676b719fdc6e375e8684712d836659f3b4e98
benjimelito <melito.ben@gmail.com>
2017-07-13 10:01:00 -0400

    Adds public folder and index.html Adds index route to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const db  = require('./db')
+const router = express.Router()
+const path = require('path')
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
 
 const port = 4000
 
