db8e693c4c9537936f9f4c82fe1910e7f53c9874
benjimelito <melito.ben@gmail.com>
2017-07-13 16:22:19 -0400

    Adds working testers route for multiple devices and countries Still not working for multiple devices and just one country

--- server/controllers/testersController.js
+++ server/controllers/testersController.js
@@ -17,22 +17,42 @@ exports.findTesters = (req, res, next) => {
     .then((testers) => {
       res.send(testers)
     })
-  }
+  } else {
 
-// If device is specified, but not country
-  if(!req.query.country && req.query.device) {
+  // If either just device, or both country and device
+  // are specified
     Device.selectByDevice(req.query.device)
     .then((tester_devices) => {
-      console.log('got these tester_devices: ', tester_devices)
+      
       const testerIds = tester_devices.map((tester_device) => {
         return tester_device.testerId
       })
 
-      console.log('mapped testerIds: ', testerIds)
-
       Tester.getTestersById(testerIds)
       .then((testers) => {
-        res.send(testers)
+        
+        // If country is specified
+        if(req.query.country){
+          
+          // If we have an array of countries
+          if (Array.isArray(req.query.country)){
+            let filteredTesters = testers.filter((tester) => {
+              return req.query.country.indexOf(tester.country) !== -1
+            })
+            res.send(filteredTesters)
+
+          // If we just have one country  
+          } else {
+            let filteredTesters = testers.filter((tester) => {
+              return tester.country = req.query.country
+            })
+            res.send(filteredTesters)
+          }
+        
+        // If country is not specified
+        } else {
+          res.send(testers)
+        }
       })
     })
   }
